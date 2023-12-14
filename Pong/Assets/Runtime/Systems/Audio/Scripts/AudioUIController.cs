4aff78bc96ce23296a65e10f7637a48f983c23c1
MarcusViniciusS <lowpoc.developer@gmail.com>
2023-12-14 15:03:41 -0300

    Feature: Settings panel

--- Pong/Assets/Runtime/Systems/Audio/Scripts/AudioUIController.cs
+++ Pong/Assets/Runtime/Systems/Audio/Scripts/AudioUIController.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,27 +6,51 @@ namespace Runtime.Systems.Audio.Scripts
 {
     public class AudioUIController : MonoBehaviour
     {
+        [Header("Sliders")]
         public Slider musicSlider, sfxSlider;
+        
+        [Header("Toggle")]
+        public Toggle toggle;
+
+        [Header("Icons")] 
+        public Sprite MusicOn, MusicOff, SoundOn, SoundOff;
+        public GameObject Music, Sfx;
+
+        [Header("Texture")] 
+        public Texture ToggleOn, ToogleOff;
 
         private void Start()
         {
+            Music.GetComponent<Image>().sprite = musicSlider.value > 0 ? MusicOn : MusicOff;
+            Sfx.GetComponent<Image>().sprite = musicSlider.value > 0 ? SoundOn : SoundOff;
+            
             AudioManager.Instance.PlayMusic("piano");
-            AudioManager.Instance.PlaySfx("sword");
+            AudioManager.Instance.PlaySfx("sword"); 
         }
 
         public void ToogleSound()
         {
             AudioManager.Instance.Toogle();
+
+            toggle.transform.Find("Image").gameObject.GetComponent<RawImage>().texture = toggle.isOn ? ToggleOn : ToogleOff;
         }
 
         public void SetVolumeMusic()
         {
             AudioManager.Instance.SetVolumeMusic(musicSlider.value);
+            
+            Music.GetComponent<Image>().sprite = musicSlider.value > 0 ? MusicOn : MusicOff;
         }
         
         public void SetVolumeSfx()
         {
             AudioManager.Instance.SetSfxMusic(sfxSlider.value);
+            Sfx.GetComponent<Image>().sprite = sfxSlider.value > 0 ? SoundOn : SoundOff;
+        }
+
+        public void ClosePainel()
+        {
+            gameObject.SetActive(false);
         }
     }
 }
